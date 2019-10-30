3cb4248989bcbef735e153c322464b1010afae98
makktim <kasatimi01@gmail.com>
2019-10-30 17:37:43 +0100

    working addNewSong

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import MusicLibrary from "./pages/MusicLibrary";
+import {BrowserRouter, Route} from 'react-router-dom';
 import NavBar from "./header/Navbar";
+import AddSong from "./pages/AddSong";
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
-    <MusicLibrary/>
+        <BrowserRouter>
+        <NavBar/>
+        <Route exact path='/' component={MusicLibrary} />
+        <Route path='/songs/add' component={AddSong} />
+        {/*<Route exact path='/songs/edit/:id' component={} />*/}
+        {/*<Route exact path='/songs/:id' component={} />*/}
+        </BrowserRouter>
     </div>
 
   );
