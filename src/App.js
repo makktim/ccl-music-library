d3fc0b3df2e3408d804143a388855c03ed140691
makktim <kasatimi01@gmail.com>
2019-11-12 14:02:52 +0100

    all music list page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import MusicLibrary from "./pages/MusicLibrary";
+import MusicList from "./pages/MusicList";
 import {BrowserRouter, Route} from 'react-router-dom';
 import NavBar from "./header/Navbar";
 import AddSong from "./pages/AddSong";
+import MusicContextProvider from "./context/MusicContext";
 
 function App() {
   return (
     <div className="App">
+        {/*<MusicContextProvider>*/}
+        {/*    <NavBar/>*/}
+        {/*</MusicContextProvider>*/}
+
+
         <BrowserRouter>
         <NavBar/>
-        <Route exact path='/' component={MusicLibrary} />
+        <Route exact path='/' component={MusicList} />
+        <Route path='/list' component={MusicLibrary} />
         <Route path='/songs/add' component={AddSong} />
         {/*<Route exact path='/songs/edit/:id' component={} />*/}
         {/*<Route exact path='/songs/:id' component={} />*/}
